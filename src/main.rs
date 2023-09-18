90c014c0170652e645d0ae35259b180b7732f7fe
Robert Spring <robertgs@authenticid.com>
2023-09-17 19:45:09 -0700

    Completed the project! Must go back to my notes and add necessary comments for future reference.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io; // Import io from the standard library and bring it into scope
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io; // Import io from the standard library and bring it into scope
 
 fn main() {
     println!("Guess the number!");
@@ -7,16 +8,30 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
+    loop {
+        println!("\nPlease input your guess.");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new(); // This line creates a mutable variable "guess" that is currently bound to a new, empty instance of a String. So guess = "" in this case.
+        let mut guess = String::new(); // This line creates a mutable variable "guess" that is currently bound to a new, empty instance of a String. So guess = "" in this case.
 
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
