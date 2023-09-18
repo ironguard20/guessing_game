115324067cb6c158cc96468dcc23e028b034cda3
Robert Spring <robertgs@authenticid.com>
2023-09-17 19:21:54 -0700

    Added rand::Rng to generate the RNG numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::io; // Import io from the standard library and bring it into scope
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
